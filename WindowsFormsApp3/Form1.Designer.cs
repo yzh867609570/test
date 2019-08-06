c2cbc3eee6755403b4def0e26bee26da499f3627
yzh <867609570@qq.com>
2019-08-06 11:34:22 +0800

    1.0

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(330, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Post请求";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(230, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(362, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(230, 115);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(362, 20);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("用户名或者密码不能为空", "登录提示");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var url = "http://120.24.220.115:8080/Lafite_war/a/automation/testAPI/interface/insertSituation";
+
+            var body = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "file", "文件"}
+                });
+            comboBox1.Text = Post(url, body);
+
+            //Dictionary<string, string> parameters = new Dictionary<string, string>();
+            //parameters.Add("file", "文件");
+            //comboBox1.Text = CreatePostHttpResponse(url, parameters).ContentLength.ToString();
+
+            //post请求并调用
+            //Dictionary<string, string> dic = new Dictionary<string, string>();
+            //dic.Add("file", "文件");
+            //comboBox1.Text = GetResponseString(CreatePostHttpResponse(url, dic));
+
+            //get请求并调用
+            //comboBox1.Text = GetResponseString(CreateGetHttpResponse(url));
+
+            //HttpDldFile df = new HttpDldFile();
+            //comboBox2.Text = df.Download(url, @"D:\Temp\1").ToString();
+
+            //comboBox2.Text = HttpDownloadFile(url, @"D:\Temp\1");
+
+            //comboBox2.Text = Post1(url);
+
+            //comboBox2.Text = PostDataGetHtml(url, "file:文件");
+
+            //comboBox2.Text = Post2(url, new Dictionary<string, string>
+            //    {
+            //        { "file", "文件"}
+            //    });
+
+            comboBox2.Text = PostData(url, "file:文件");
+
+        }
+
+        /// <summary>
+        /// POST传值
+        /// </summary>
+        /// <param name="posturl">传递的地址</param>
+        /// <param name="postData">传递的内容（如：a=1&b=2&c=3）</param>
+        /// <returns>返回结果</returns>
+        public string PostData(string posturl, string postData)
+        {
+            Stream outstream = null;
+            Stream instream = null;
+            StreamReader sr = null;
+            HttpWebResponse response = null;
+            HttpWebRequest request = null;
+            Encoding encoding = System.Text.Encoding.GetEncoding("UTF-8");
+            byte[] data = encoding.GetBytes(postData);
+            // 准备请求...
+            try
+            {
+                // 设置参数
+                request = WebRequest.Create(posturl) as HttpWebRequest;
+                CookieContainer cookieContainer = new CookieContainer();
+                request.CookieContainer = cookieContainer;
+                request.AllowAutoRedirect = true;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                outstream = request.GetRequestStream();
+                outstream.Write(data, 0, data.Length);
+                outstream.Close();
+                //发送请求并获取相应回应数据
+                response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求
+                instream = response.GetResponseStream();
+                sr = new StreamReader(instream, encoding);
+                //返回结果网页（html）代码
+                string content = sr.ReadToEnd();
+                string err = string.Empty;
+                return content;
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message;
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 指定Post地址使用Get 方式获取全部字符串
+        /// </summary>
+        /// <param name="url">请求后台地址</param>
+        /// <returns></returns>
+        private string Post2(string url, Dictionary<string, string> dic)
+        {
+            string result = string.Empty;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+
+            #region 添加Post 参数
+            StringBuilder builder = new StringBuilder();
+            int i = 0;
+            foreach (var item in dic)
+            {
+                if (i > 0)
+                    builder.Append("&");
+                builder.AppendFormat("{0}={1}", item.Key, item.Value);
+                i++;
+            }
+            byte[] data = Encoding.UTF8.GetBytes(builder.ToString());
+            req.ContentLength = data.Length;
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+                reqStream.Close();
+            }
+            #endregion
+
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            //获取响应内容
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// HttpWebRequest 通过Post
+        /// </summary>
+        /// <param name="url">URI</param>
+        /// <param name="postData">post数据</param>
+        /// <returns></returns>
+        public static string PostDataGetHtml(string url, string postData)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(postData);
+
+                Uri uri = new Uri(url);
+                HttpWebRequest req = WebRequest.Create(uri) as HttpWebRequest;
+                if (req == null)
+                {
+                    return "Network error:" + new ArgumentNullException("httpWebRequest").Message;
+                }
+                req.Method = "POST";
+                req.KeepAlive = true;
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.ContentLength = data.Length;
+                req.AllowAutoRedirect = true;
+
+                Stream outStream = req.GetRequestStream();
+                outStream.Write(data, 0, data.Length);
+                outStream.Close();
+
+                var res = req.GetResponse() as HttpWebResponse;
+                if (res == null)
+                {
+                    return "Network error:" + new ArgumentNullException("HttpWebResponse").Message;
+                }
+                Stream inStream = res.GetResponseStream();
+                var sr = new StreamReader(inStream, Encoding.UTF8);
+                string htmlResult = sr.ReadToEnd();
+
+                return htmlResult;
+            }
+            catch (Exception ex)
+            {
+                return "网络错误(Network error)：" + ex.Message;
+            }
+        }
+
+        public string Post1(string url)
+        {
+            #region 定义请求体中的内容 并转成二进制
+            string urlBody = @"file:文件";
+
+            var modelIdStrByte = Encoding.UTF8.GetBytes(urlBody);//modelId所有字符串二进制
+            #endregion
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+
+            Stream myRequestStream = request.GetRequestStream();//定义请求流
+
+            //将各个二进制 安顺序写入请求流 modelIdStr -> (fileContentStr + fileContent) -> uodateTimeStr -> encryptStr
+            myRequestStream.Write(modelIdStrByte, 0, modelIdStrByte.Length);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();//发送
+
+            Stream myResponseStream = response.GetResponseStream();//获取返回值
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+
+            string retString = myStreamReader.ReadToEnd();
+
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+
+        public string Post(string url, FormUrlEncodedContent body)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Accept", "application/json");//设置请求头
+                body.Headers.ContentType.CharSet = "utf-8";
+                // response
+                var response = httpClient.PostAsync(url, body).Result;
+                var data = response.Content.ReadAsStringAsync().Result;
+                return data;//接口调用成功数据
+            }
+        }
+
+        /// <summary>
+        /// 发送http post请求
+        /// </summary>
+        /// <param name="url">地址</param>
+        /// <param name="parameters">查询参数集合</param>
+        /// <returns></returns>
+        public HttpWebResponse CreatePostHttpResponse(string url, IDictionary<string, string> parameters)
+        {
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;//创建请求对象
+            request.Method = "POST";//请求方式
+            request.ContentType = "application/x-www-form-urlencoded";//链接类型
+                                                                      //构造查询字符串
+            if (!(parameters == null || parameters.Count == 0))
+            {
+                StringBuilder buffer = new StringBuilder();
+                bool first = true;
+                foreach (string key in parameters.Keys)
+                {
+
+                    if (!first)
+                    {
+                        buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                    }
+                    else
+                    {
+                        buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                        first = false;
+                    }
+                }
+                byte[] data = Encoding.UTF8.GetBytes(buffer.ToString());
+                //写入请求流
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            return request.GetResponse() as HttpWebResponse;
+        }
+
+        /// <summary>
+        /// 发送http Get请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static HttpWebResponse CreateGetHttpResponse(string url)
+        {
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "GET";
+            request.ContentType = "application/x-www-form-urlencoded";//链接类型
+            return request.GetResponse() as HttpWebResponse;
+        }
+
+        /// <summary>
+        /// 从HttpWebResponse对象中提取响应的数据转换为字符串
+        /// </summary>
+        /// <param name="webresponse"></param>
+        /// <returns></returns>
+        public string GetResponseString(HttpWebResponse webresponse)
+        {
+            using (Stream s = webresponse.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(s, Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+        }
+
+    }
+}
